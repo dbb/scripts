5d0d05568faee5b8c5ffeff0d462e5731786d402
Daniel Bolton <danielbarrettbolton@gmail.com>
2010-02-10 20:51:59 -0500

    updating apt-file.pl

--- apt-carbon.pl
+++ apt-carbon.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 use autodie;
-use List::MoreUtils qw(natatime);
+use List::MoreUtils qw(natatime); # debian: liblist-moreutils-perl
 
 # dbbolton
 # danielbarrettbolton@gmail.com
