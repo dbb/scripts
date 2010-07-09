5b67628f78469657ae27ea17d6a35257dcab8469
Daniel Bolton <danielbarrettbolton@gmail.com>
2010-07-09 00:59:59 -0400

    fixed some quirks in file-rename.pl

--- file-rename.pl
+++ file-rename.pl
@@ -14,29 +14,40 @@ opendir( my $dh, $path );
 my @files = sort( readdir($dh) );
 
 my $new;
-foreach (@files) {
+for (@files) {
     chomp;
     unless (/^[.]+$/) {
         $new = fixname($_);
         if ( $_ ne $new ) {
+            print "Attempting to fix '$_' ------------\n";
             my $i=0;
+            my $old_num;
+            my $new_num;
             while ( -f $new ) {
-                if (/(\.[\w]+)$/) {
+                print "\t'$new' already exists here\n";
+                if ( /(\d+)\./ or /(\d+)$/ ) {
+                    print "\tUsing '$1' as an identifying number\n";
+                    $old_num = $1;
+                    $new_num = $old_num + 1;
+                    $new =~ s/\Q$old_num\E/$new_num/;
+                }
+                elsif ( /(\.[\w]+)$/ ) {
+                    print "\tUsing '$1' as the extension\n";
                     my $ext = $1;
                     $new =~ s/\Q$ext\E/\-$i$ext/;
                     $i++;
                 }
                 else {
-                    print "no file ext found\n";
-                    $new .= $i;
+                    print "\tNo file ext found\n";
+                    $new .= "-$i";
                     $i++;
                 }
             }
-            print "old: $_";
-            print "new: $new\n";
+            print "\told: $_\n";
+            print "\tnew: $new\n";
             move($_, $new);
         }
-        else { print "\"$_\" is OK."; }
+        else { print "'$_' is OK.\n"; }
     }
 
 }
