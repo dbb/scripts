1a61f65b6e934472f0ee73269c5967ccabd92085
Daniel Bolton <danielbarrettbolton@gmail.com>
2011-02-06 22:36:04 -0500

    Add new mem info sub

--- dwm-status.pl
+++ dwm-status.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl 
 use autodie;
 use Date::Format;
 use strict;
@@ -7,27 +7,57 @@ use warnings;
 # github.com/dbbolton
 
 while (1) {
-# Current Load #############################################################
-chop( my $load = (split ' ', `uptime`)[-2] );
+    my $status = &load . ' | ' . &freem . ' MB | ' . &clock;
 
-# Active Memory ############################################################
-my $memmb = 0;
-open(my $meminfo,  "<",  "/proc/meminfo");
-while ( <$meminfo> ) {
-    if ( /^Active:/ ) {
-        my $memkb = ( split(/ /, $_) )[-2];
-        $memmb = sprintf( "%.0f", ($memkb/1024) );
-    }
-}
+    # where the text is printed
+    system "xsetroot -name \"$status\"" ;
 
-# Time #####################################################################
-my $zeit = time2str( "%R", time );
-
-# All together now #########################################################
-my $status = "$load | $memmb MB | $zeit ";
-
-system "xsetroot -name \"$status\"" ;
-system "sleep 5s";
+    # this is used for testing in the console
+    # you might want to comment out the infinite loop's start and end
+    # as well
+    # print $status;
+    
+    # update interval
+    system "sleep 5s";
 
 } # end infinite loop
+
+# Current Load ############################################################
+sub load {
+    chop( my $load = (split ' ', `uptime`)[-2] );
+    return $load;
+}
+# Active Memory ###########################################################
+sub act_mem {
+    my $memmb = 0;
+
+    open(my $meminfo,  "<",  "/proc/meminfo");
+
+    while ( <$meminfo> ) {
+        if ( /^Active:/ ) {
+            my $memkb = ( split(/ /, $_) )[-2];
+            $memmb = sprintf( "%.0f", ($memkb/1024) );
+            return $memmb;
+        } # end if
+    } # end while
+} # end &act_mem
+
+# Used mem from `free -m` #################################################
+sub freem {
+    my @lines = split /\n/, `free -m`;
+
+    for ( @lines ) {
+        if ( /cache:/ ) {
+           my $used = (split /\s+/, $_)[2];
+           return $used;
+       } # end if
+    } # end for
+} # end &freem
+
+
+# Time #####################################################################
+sub clock {
+    my $time = time2str( "%R", time );
+    return $time;
+}
 
